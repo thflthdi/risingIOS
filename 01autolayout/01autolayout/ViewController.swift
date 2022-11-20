38a7efa3ff0700ec53894388eb833e897d2ec3a2
thflthdi <thflthdi@gmail.com>
2022-11-21 04:52:37 +0900

    메인 스크롤뷰, Top 10 횡 스크롤뷰 구현

--- 01autolayout/01autolayout/ViewController.swift
+++ 01autolayout/01autolayout/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var MainVerticalScrollView: UIScrollView!
+    
+    @IBOutlet weak var KRHorizScrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        MainVerticalScrollView.showsVerticalScrollIndicator = false
+        KRHorizScrollView.showsHorizontalScrollIndicator = false
     }
 
 
