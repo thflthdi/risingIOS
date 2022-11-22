53bfa743c2ca842370f6b0b4cb961129adfa70ed
thflthdi <thflthdi@gmail.com>
2022-11-23 01:51:00 +0900

    배너 화면 구현

--- 01autolayout/01autolayout/ViewController.swift
+++ 01autolayout/01autolayout/ViewController.swift
@@ -12,12 +12,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var KRHorizScrollView: UIScrollView!
     
+    @IBOutlet weak var bannerView: UIView!
+    
+    @IBOutlet weak var descriptionView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         MainVerticalScrollView.showsVerticalScrollIndicator = false
         KRHorizScrollView.showsHorizontalScrollIndicator = false
+//        descriptionView.setGradient(color1: UIColor.lightGray, color2: .black)
     }
-
+    
 
 }
 
+//extension UIView {
+//    func setGradient(color1:UIColor, color2:UIColor){
+//        let gradient: CAGradientLayer = CAGradientLayer()
+//        gradient.colors = [color1.cgColor, color2.cgColor]
+//        gradient.locations = [0.8 , 1.0]
+//                gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+//                gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+//                gradient.frame = bounds
+//                layer.addSublayer(gradient)
+//    }
+//}
