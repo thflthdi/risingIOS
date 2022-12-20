e1b2ba6028b0d1bf265badec93d64a4d3512531b
thflthdi <thflthdi@gmail.com>
2022-12-20 20:09:41 +0900

    open api 이용한 프로젝트

---
+++ 05API/05API/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  05API
+//
+//  Created by 황예진 on 2022/12/20.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 앱에서 위치 관련 이벤트 전달을 시작/중지 하는데 사용하는 개체
+        var localtionManager: CLLocationManager?
+        // 위도와 경도를 알려주는 struct
+        var currentLocation: CLLocationCoordinate2D!
+    }
+
+
+}
+
